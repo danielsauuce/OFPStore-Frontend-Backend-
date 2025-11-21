f074b205808a060f135400a43429b01806dee89a
Daniel <danieladeyd77@gmail.com>
2025-11-21 18:32:50 +0000

    winston and Joi install for logging and validation.. Userschema modified

--- server/models/user.mjs
+++ server/models/user.mjs
@@ -6,6 +6,8 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String, required: true },
     username: { type: String, required: true, unique: true, trim: true },
+    firstname: { type: String, required: true, unique: true, trim: true },
+    lastname: { type: String, required: true, unique: true, trim: true },
     role: { type: String, enum: ['customer', 'admin'], default: 'customer' },
     isActive: { type: Boolean, default: true },
     lastLogin: { type: Date },
