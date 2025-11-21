adf09b7ac919dd612264e8ac72b55f38e246aa73
Daniel <danieladeyd77@gmail.com>
2025-11-21 18:24:48 +0000

    cart schema added

---
+++ server/models/cart.mjs
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+
+const cartItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+    default: 1
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  }
+}, {
+  _id: false
+});
+
+const cartSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    unique: true
+  },
+  items: [cartItemSchema],
+  total: {
+    type: Number,
+    default: 0,
+    min: 0
+  },
+  lastModified: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true
+});
+
+// Index for faster queries
+cartSchema.index({ user: 1 });
+
+// Update lastModified on save
+cartSchema.pre('save', function(next) {
+  this.lastModified = new Date();
+  next();
+});
+
+export default mongoose.model('Cart', cartSchema);
