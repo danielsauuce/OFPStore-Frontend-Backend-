f7515ba6eb4a07acf2a83f8aa5b02d25fbd0478b
Daniel <danieladeyd77@gmail.com>
2025-11-27 13:51:46 +0000

    Admin page modified

--- client/src/views/admin/AdminLayout.jsx
+++ client/src/views/admin/AdminLayout.jsx
@@ -5,14 +5,14 @@ import { ReactNode } from 'react';
 
 // const isActive = (path: string) => location.pathname === path;
 
-const AdminLayout = ({children}) => {
+const AdminLayout = ({ children }) => {
   const location = useLocation();
   const navigation = navigations;
 
   return (
     <div className="min-h-screen bg-muted/30 flex">
       {/* Sidebar */}
-      <aside className="w-64 min-h-screen bg-card border-r">
+      <aside className="w-64 min-h-screen bg-card border-r border-primary ">
         <div className="p-6">
           {/* Back to Store */}
           <Link
@@ -26,7 +26,23 @@ const AdminLayout = ({children}) => {
           {/* Sidebar Title */}
           <h2 className="text-xl font-serif font-bold mb-6">Admin Panel</h2>
 
-          
+          {/* Navigation */}
+          {/* <nav className="space-y-2">
+            {navigation.map((item) => {
+              const Icon = item.icon;
+              return (
+                <Link key={item.name} to={item.href}>
+                  <div
+                    className={`flex items-center gap-2 px-4 py-2 rounded-lg cursor-pointer transition-colors
+                      ${isActive(item.href) ? 'bg-primary text-primary-foreground' : 'text-muted-foreground hover:bg-primary/10 hover:text-primary'}`}
+                  >
+                    <Icon className="h-5 w-5" />
+                    <span>{item.name}</span>
+                  </div>
+                </Link>
+              );
+            })}
+          </nav> */}
         </div>
       </aside>
 
