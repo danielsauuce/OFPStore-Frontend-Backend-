a64bb3253d2f7eb3deb34c9fc77668ed7beb4d8b
Daniel Omogbolahan Adeyemi <6adeyd77@m-b118-79160.uni.solent.ac.uk>
2025-12-05 21:19:52 +0000

    expo reset and tailwind configured

---
+++ mobile/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import '../global.css'
+
+export default function Index() {
+  return (
+    <View className="min-h-screen mt-2"
+    
+    >
+      <Text className="bg-red-100 p-3 border">Olayinka furniture palace </Text>
+    </View>
+  );
+}
