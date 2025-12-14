4e2571a920b3a73590791a81816daff63b14178d
danielsauuce <danieladeyd77@gmail.com>
2025-12-14 18:15:53 +0000

    Still fixing HomeScreen

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
-import { ScrollView, Text, View, Image } from 'react-native';
-import { ArrowRight, ShoppingBag, Star } from 'lucide-react-native';
+import { ScrollView, Text, View } from 'react-native';
 import { useRouter } from 'expo-router';
+import HeroSection from '../../src/components/heroSection';
 
-export default function Index() {
+const Index = () => {
   const router = useRouter();
+
   return (
-    <View className="min-h-screen bg-light-background flex-1">
+    <View className="flex-1 bg-light-background min-h-screen">
       <ScrollView showsVerticalScrollIndicator={false}>
         {/* Header */}
         <View className="px-5 pt-5 pb-2 items-center">
@@ -16,35 +17,15 @@ export default function Index() {
         </View>
 
         {/* Hero Section */}
-        <View className=" px-5 py-4 mt-5">
-          <View className=" rounded-3xl overflow-hidden h-80 flex-row bg-light-primary">
-            <View className="flex-1 p-7 justify-center">
-              <Text className="text-4xl font-bold text-white leading-tight mb-2">
-                Discover{'\n'}Timeless{'\n'}Elegance
-              </Text>
-              <Text className="text-white/90 text-sm mb-6">
-                Curated furniture for your dream home
-              </Text>
+        <HeroSection onPress={() => router.push('/shop')} buttonText="Shop Now" />
 
-              <View
-                className={`flex-row items-center self-start px-5 py-3 rounded-2xl bg-light-surface gap-2`}
-              >
-                <Text className={`text-light-primary font-semibold`}>Shop Now</Text>
-                <ArrowRight size={18} className="text-light-primary" />
-              </View>
-            </View>
-            <Image
-              source={{
-                uri: 'https://i.pinimg.com/1200x/78/18/45/781845e16c300760d3ad9c17bc5994a6.jpg',
-              }}
-              className="w-36 h-full"
-            />
-          </View>
+        {/* Featured Collection Placeholder */}
+        <View className="border border-red-500 px-5 py-6 mt-5">
+          <Text className="text-light-textSecondary">Featured Collection Coming Soon</Text>
         </View>
-
-        {/* Featured Collection */}
-        <View className="border border-red-500 px-5 py-6 mt-5"></View>
       </ScrollView>
     </View>
   );
-}
+};
+
+export default Index;
---
+++ mobile/src/types/HeroSection.tsx
@@ -0,0 +1,7 @@
+export type HeroSectionProps = {
+  title: string;
+  subtitle: string;
+  imageUrl: string;
+  buttonText: string;
+  onPress: () => void;
+};
